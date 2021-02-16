46608eef97abe93dd70e11a56a990500c1c052d8
honey2020 <garaulavinia@gmail.com>
2021-02-16 10:27:41 +0000

    final

--- src/main/java/exercise0/Persona.java
+++ src/main/java/exercise0/Persona.java
@@ -2,15 +2,24 @@ package exercise0;
 
 
 public class Persona {
-    String nome;
-    String cognome;
+    String nome, cognome;
     int eta;
 
 
-    public String dettagli(nome, cognome, eta){
-        String pNome = this.nome;
-        String pCognome = this.cognome;
-        String pEta = this.eta.toString();
-        return pNome + pCognome + pEta;
+    public Persona (){
+      this.nome = "hello";
+      this.cognome= "world";
+      this.eta = 0;
+    }
+
+    public Persona(String nome, String cognome, int eta){
+      this.nome = nome;
+      this.cognome = cognome;
+      this.eta = eta;
+    }
+
+    public String dettagli(){
+        String d = "La persona si chiama " + this.nome + " " + this.cognome + " e ha " + this.eta + " anni."
+        return d;
     }
 }
