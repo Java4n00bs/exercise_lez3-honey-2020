4a02fbf554db342d1c366b8122aa5a1985440c07
honey2020 <garaulavinia@gmail.com>
2021-02-16 10:06:37 +0000

    starter

--- src/main/java/exercise0/Persona.java
+++ src/main/java/exercise0/Persona.java
@@ -2,9 +2,15 @@ package exercise0;
 
 
 public class Persona {
+    String nome;
+    String cognome;
+    int eta;
 
-    public String dettagli(){
 
-        return "";
+    public String dettagli(nome, cognome, eta){
+        String pNome = this.nome;
+        String pCognome = this.cognome;
+        String pEta = this.eta.toString();
+        return pNome + pCognome + pEta;
     }
 }
